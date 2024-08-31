fbd726b1f0792f403f867affa65ac94863664a13
Eric Auld <ericauld@together.ai>
2024-08-31 09:01:30 -0700

    Add profiler build script

---
+++ build_profiler.sh
@@ -0,0 +1,8 @@
+cd build \
+&& cmake .. -DCUTLASS_NVCC_ARCHS=90a \
+    -DCUTLASS_ENABLE_TESTS=OFF \
+    -DCUTLASS_UNITY_BUILD_ENABLED=ON \
+    -DCUTLASS_LIBRARY_OPERATIONS=gemm \
+    -DCUTLASS_LIBRARY_KERNELS="cutlass3x*bf16*" \
+    -DCUTLASS_ENABLE_CUBLAS=ON  \
+&& make cutlass_profiler -j64
